abe5bc7874c8ea30216cd2c2bcb7a39277e30a5c
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-11-15 21:30:38 +0800

    create table permenant invoice

---
+++ database/migrations/2019_11_15_210113_create_invoice_permenant_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicePermenantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_permenant', function (Blueprint $table) {
+            $table->bigIncrements('ip_id');
+            $table->unsignedBigInteger('i_id')->unsigned();
+            $table->unsignedBigInteger('s_id')->unsigned();
+            $table->integer('price_unit');
+            $table->integer('quantity');
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_permenant');
+    }
+}
