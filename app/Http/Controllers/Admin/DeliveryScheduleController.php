1f7516ceb8c81591673c37ec478001189bc8f8e8
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-11-19 17:03:48 +0800

    -delivery schedule controller

--- app/Http/Controllers/Admin/DeliveryScheduleController.php
+++ app/Http/Controllers/Admin/DeliveryScheduleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class DeliveryScheduleController extends Controller
 {
@@ -15,7 +16,10 @@ class DeliveryScheduleController extends Controller
     
     public function viewSchedule()
     {
+        $orders = DB::table('orders')
+                    ->where('o_status','<>',9)
+                    ->get();
         
-        return view('admin/delivery_schedule');
+        return view('admin/delivery_schedule',compact('orders'));
     }
 }
