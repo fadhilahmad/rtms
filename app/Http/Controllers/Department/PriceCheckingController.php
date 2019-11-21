6bc03f25f419d55532c202982c6057eca8defdb4
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-11-21 19:02:10 +0800

    price checking controller

---
+++ app/Http/Controllers/Department/PriceCheckingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Department;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use DB;
+
+class PriceCheckingController extends Controller
+{
+    //
+    public function BaseOnBodyId(Request $request) 
+    {
+          $data = $request->all();
+          
+          $sl_info =  DB::table('price')
+                       ->join('sleeve', 'price.sl_id', '=', 'sleeve.sl_id')
+                       ->where('price.sl_id','=',$data['bid'])
+                       ->get();
+          
+               foreach ($sl_info as $sl){
+                   $slid[]=$sl->sl_id;
+                   $sldesc[]=$sl->sl_desc;
+               }
+          
+          return response()->json(['slid'=>$slid,'sldesc'=>$sldesc]);
+    }   
+}
