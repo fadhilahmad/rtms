db75da7cecae896ec8b39642bdc26107a58bab9f
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-11-21 18:14:59 +0800

    -department middleware

--- app/Http/Middleware/Department.php
+++ app/Http/Middleware/Department.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Session\Store;
+use Illuminate\Support\Facades\Auth;
 
 class Department
 {
@@ -15,9 +17,22 @@ class Department
      */
     public function handle($request, Closure $next)
     {
-        if(auth()->user()->u_type == 3 OR auth()->user()->u_type == 4 OR auth()->user()->u_type == 5 AND auth()->user()->u_status == 1 ){
-        return $next($request);
-    }
+        if(Auth::check())
+        {
+            
+            if(auth()->user()->u_status == 1)
+            {  
+                
+                if(auth()->user()->u_type == 3 OR auth()->user()->u_type == 4 OR auth()->user()->u_type == 5 )
+                {
+                    return $next($request);
+                }
+            }
+            else
+            {
+                return redirect('home')->with('error','You do not have access');
+            }
+        }
         return redirect('login')->with('error','You do not have access');
     }
 }
