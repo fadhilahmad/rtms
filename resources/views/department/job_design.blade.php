9f5e70c1471ec411e366a2b77be653dec52ecf57
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-11-25 02:10:02 +0800

    design department job_design view updated

--- resources/views/department/job_design.blade.php
+++ resources/views/department/job_design.blade.php
@@ -30,32 +30,18 @@ text-align: center;
                                         <div class="col-sm-3">File name</div>
                                         <div class="col-sm-1">:</div>
                                         <div class="col-sm-8">{{$order->file_name}}</div>
-                                    </div><br>
-                                    <div class="row">
-                                        <div class="col-sm-3">Category</div>
-                                        <div class="col-sm-1">:</div>
-                                        <div class="col-sm-8">{{$order->category}}</div>
                                     </div><br> 
                                     <div class="row">
                                         <div class="col-sm-3">Total Quantity</div>
                                         <div class="col-sm-1">:</div>
                                         <div class="col-sm-8">{{$order->quantity_total}}</div>
-                                    </div><br>
-                                    <div class="row">
-                                        <div class="col-sm-3">Material</div>
-                                        <div class="col-sm-1">:</div>
-                                        <div class="col-sm-8">{{$order->m_desc}}</div>
                                     </div><br>                                    
                                     <div class="row">
                                         <div class="col-sm-3">Note</div>
                                         <div class="col-sm-1">:</div>
                                         <div class="col-sm-8">{{$order->note}}</div>
                                     </div><br>
-                                    <div class="row">
-                                        <div class="col-sm-3">Delivery Date</div>
-                                        <div class="col-sm-1">:</div>
-                                        <div class="col-sm-8">{{$order->delivery_date}}</div>
-                                    </div><br>
+                            @if($order->category=="Size")
                             @php $no=0; @endphp
                             @foreach($specs as $spec)
                             <div class="panel panel-primary">
@@ -64,32 +50,25 @@ text-align: center;
                                     <table class="table table-hover table-bordered">
                                         <thead class="thead-dark">
                                           <tr>
-                                            <th scope="col">Name</th>
                                             <th scope="col">Size</th>
                                             <th scope="col">Quantity</th>
-                                            <th scope="col">Upload</th>
+                                            <th scope="col">Designed</th>
                                           </tr>
                                         </thead>
                                         <tbody> 
                                     
                                     @foreach($units->where('o_id',$spec->o_id)->where('s_id',$spec->s_id) as $unit)
                                            <tr>
-                                              <td>{{$unit->name}}</td>
                                               <td style="text-transform: uppercase;">{{$unit->size}}</td>
                                               <td>{{$unit->un_quantity}}</td>
                                               <td>
-                                                  @if($design->where('o_id',$spec->o_id)->where('un_id',$unit->un_id)->where('d_type','3')->count()>0)
-                                                  Uploaded
+                                                  @if($units->where('o_id',$spec->o_id)->where('un_id',$unit->un_id)->where('un_status','0')->count()>0)
+                                                  <button 
+                                                        class="btn btn-primary done" data-target="#Modal" data-table="body" data-uid="{{Auth::id()}}" data-process="design"
+                                                        data-oid="{{$unit->o_id}}" data-unid="{{$unit->un_id}}">Completed
+                                                  </button>
                                                   @else
-                                                  <form enctype="multipart/form-data" method="post" action="{{route('update.design')}}">
-                                                  @csrf                                           
-                                                  <input type="file" name="job" required="" >
-                                                  <input type="hidden" name="o_id" value="{{$unit->o_id}}">
-                                                  <input type="hidden" name="un_id" value="{{$unit->un_id}}">
-                                                  <input type="hidden" name="u_id" value="{{Auth::id()}}">
-                                                  <input type="hidden" name="process" value="design">
-                                                  <button type="submit" class="btn btn-primary edit" >Upload</button>
-                                                  </form>
+                                                  <input type="checkbox" name="jobdone" value="" checked="" disabled="">
                                                   @endif
                                               </td>                                                                                                               
                                           </tr>
@@ -101,10 +80,57 @@ text-align: center;
                             </div>
                             
                             @php
-                            $completed =  $design->where('o_id',$spec->o_id)->where('d_type','3')->count()
+                            $completed =  $units->where('o_id',$spec->o_id)->where('un_status','1')->count()
                             @endphp
                             @endforeach       
-                                
+                            @endif
+                            
+                            @if($order->category=="Nameset")
+                            @php $no=0; @endphp
+                            @foreach($specs as $spec)
+                            <div class="panel panel-primary">
+                                <br><br><div class="panel-heading"><center><h2 style="text-transform: uppercase;">{{$spec->b_desc}} {{$spec->sl_desc}} {{$spec->n_desc}}</h2></center></div><br><br>                                                                  
+                                <div class="panel-body">
+                                    <table class="table table-hover table-bordered">
+                                        <thead class="thead-dark">
+                                          <tr>
+                                            <th scope="col">Name/Number</th>
+                                            <th scope="col">Size</th>
+                                            <th scope="col">Quantity</th>
+                                            <th scope="col">Designed</th>
+                                          </tr>
+                                        </thead>
+                                        <tbody> 
+                                    
+                                    @foreach($units->where('o_id',$spec->o_id)->where('s_id',$spec->s_id) as $unit)
+                                           <tr>
+                                              <td>{{$unit->name}}</td>
+                                              <td style="text-transform: uppercase;">{{$unit->size}}</td>
+                                              <td>{{$unit->un_quantity}}</td>
+                                              <td>
+                                                  @if($units->where('o_id',$spec->o_id)->where('un_id',$unit->un_id)->where('un_status','0')->count()>0)
+                                                  <button 
+                                                        class="btn btn-primary done" data-target="#Modal" data-table="body" data-uid="{{Auth::id()}}" data-process="design"
+                                                        data-oid="{{$unit->o_id}}" data-unid="{{$unit->un_id}}">Completed
+                                                  </button>
+                                                  @else
+                                                  <input type="checkbox" name="jobdone" value="" checked="" disabled="">
+                                                  @endif
+                                              </td>                                                                                                               
+                                          </tr>
+                                    @php $no++; @endphp
+                                    @endforeach                                                                                   
+                                        </tbody>
+                                    </table>                                                                        
+                                </div>                                   
+                            </div>
+                            
+                            @php
+                            $completed =  $units->where('o_id',$spec->o_id)->where('un_status','1')->count()
+                            @endphp
+                            @endforeach
+                            
+                            @endif
                              <br><br>
                             @if($no==$completed)
                             <form method="post" action="{{route('update.design')}}">@csrf
@@ -120,4 +146,41 @@ text-align: center;
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    $(".done").click(function(e){
+        e.preventDefault();
+        
+        var unid = $(this).data('unid');
+        var oid = $(this).data('oid');
+        var uid = $(this).data('uid');
+        var process = $(this).data('process');
+       // console.log(unid,oid,uid,process);
+       
+        var formData = {
+            'un_id'   : unid,
+            'o_id'   : oid,
+            'u_id'    : uid,
+            'process'   : process
+        };
+
+        $.ajax({
+           type:'POST',
+           url:"{{url('department/job_design')}}",
+           data:formData,
+           success:function(data){
+               location.reload();
+           }
+        });
+      
+    });
+    
+</script>
 @endsection
