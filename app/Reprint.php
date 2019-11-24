ee5b9bee6d36557ecbb4648932410acc9384fb90
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-11-25 02:01:16 +0800

    reprint model

---
+++ app/Reprint.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reprint extends Model
+{
+    //
+    protected $table = 'reprint';
+    protected $primaryKey = 'r_id';
+}
