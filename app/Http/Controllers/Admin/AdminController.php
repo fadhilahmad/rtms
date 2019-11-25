c67bbafe9fc93a6c92cec22dec9fdb52700336d6
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-11-25 16:35:47 +0800

    admin display only uncomplete order

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -171,6 +171,7 @@ class AdminController extends Controller
     {
         $order = DB::table('orders')
                     ->Join('user', 'orders.u_id_customer', '=', 'user.u_id')
+                    ->where('orders.o_status','<>','9')
                     ->orderBy('orders.delivery_date', 'asc')
                     ->paginate(30);
         
