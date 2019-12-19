792355e5963b9c97412a09d61cbefa9964f8fc65
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-20 04:25:44 +0800

    create table block date

---
+++ database/migrations/2019_12_20_030447_create_table_blockdate.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBlockdate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('block_date', function (Blueprint $table) {
+            $table->bigIncrements('bdt_id');
+            $table->date('date')->nullable();
+            $table->string('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('block_date');
+    }
+}
