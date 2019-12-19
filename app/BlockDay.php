fd6cf404ffcc8ffe9ab6b9bee360fdf0bd97cdd1
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-20 04:28:20 +0800

    block day model

---
+++ app/BlockDay.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlockDay extends Model
+{
+    //
+    protected $table = 'block_day';
+    protected $primaryKey = 'bd_id';
+}
