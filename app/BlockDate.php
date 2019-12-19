6e47c882d93a35ef8edf40a4767a9e36b7d2083c
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-20 04:26:06 +0800

    block date model

---
+++ app/BlockDate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlockDate extends Model
+{
+    //
+    protected $table = 'block_date';
+    protected $primaryKey = 'bdt_id';
+}
