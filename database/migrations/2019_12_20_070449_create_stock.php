581ab5198f8400becbbb32249cfa8cda477abca3
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-21 20:02:06 +0800

    stock migration

---
+++ database/migrations/2019_12_20_070449_create_stock.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStock extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->bigIncrements('st_id');
+            $table->string('item');
+            $table->integer('st_quantity');
+            $table->integer('st_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
