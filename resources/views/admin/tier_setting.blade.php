b2479e6b289b16dd13cad70efa8b16c08573b65a
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-23 21:45:52 +0800

    tier setting view

---
+++ resources/views/admin/tier_setting.blade.php
@@ -0,0 +1,16 @@
+@extends ('layouts.layout')
+@section ('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Tier Setting</div>
+
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
