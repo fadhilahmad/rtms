2753d9d378c1e71a6b775baceab1975cf4ee9533
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-23 22:15:17 +0800

    contact number model

---
+++ app/ContactNumber.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactNumber extends Model
+{
+    //
+    protected $table = 'contact_number';
+    protected $primaryKey = 'cn_id';
+}
