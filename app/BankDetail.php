3c6c7af88bc59314b5069a6a79c90f1013d7df63
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-23 22:14:49 +0800

    bank detail model

---
+++ app/BankDetail.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BankDetail extends Model
+{
+    //
+    protected $table = 'bank_detail';
+    protected $primaryKey = 'bd_id';
+}
