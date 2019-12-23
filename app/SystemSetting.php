daa3863ffbdef1ec4b63bd2e48f5d8995b7ce7be
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-23 22:14:05 +0800

    system setting model

---
+++ app/SystemSetting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SystemSetting extends Model
+{
+    //
+    protected $table = 'system_setting';
+    protected $primaryKey = 'ss_id';
+}
