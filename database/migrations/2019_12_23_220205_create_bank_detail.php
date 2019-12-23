3731a2d127a3baf641b0a103a0295fcf1bf87170
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-23 22:14:30 +0800

    bank detail migration

---
+++ database/migrations/2019_12_23_220205_create_bank_detail.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBankDetail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bank_detail', function (Blueprint $table) {
+            $table->bigIncrements('bd_id');
+            $table->string('bank_name');
+            $table->string('account_name');
+            $table->integer('account_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bank_detail');
+    }
+}
