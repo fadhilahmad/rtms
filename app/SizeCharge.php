84321bbaa0c4ac1ba0cbabe9de545676da727e0b
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-23 22:16:50 +0800

    size charge model

---
+++ app/SizeCharge.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SizeCharge extends Model
+{
+    //
+    protected $table = 'size_charge';
+    protected $primaryKey = 'sc_id';
+}
