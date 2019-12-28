d87f8742d6f9fca64e4d0ffbf9a199f0a7d930d4
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-29 07:46:15 +0800

    delivery schedule controller

--- app/Http/Controllers/Admin/DeliveryScheduleController.php
+++ app/Http/Controllers/Admin/DeliveryScheduleController.php
@@ -16,19 +16,39 @@ class DeliveryScheduleController extends Controller
     
     public function viewSchedule()
     {
+        $today = date("Y-m-d");
+        
         $orders = DB::table('orders')
                     ->where('o_status','<>',9)
+                    ->where('active','=',1)
+                    ->whereDate('delivery_date','>=',$today)
                     ->get();
         
         $total = DB::table('orders')
                     ->selectRaw('*, sum(quantity_total) as sum')
                     ->where('o_status','<>',9)
+                    ->where('active','=',1)
+                    ->whereDate('delivery_date','>=',$today)
                     ->groupBy('delivery_date')
 //                    ->sum('quantity_total');
                     ->get();
         
        // dd($total);
+        $lates = DB::table('orders')
+                    ->where('o_status','<>',9)
+                    ->where('active','=',1)
+                    ->whereDate('delivery_date','<',$today)
+                    ->get();
         
-        return view('admin/delivery_schedule',compact('orders','total'));
+        $tot_lates = DB::table('orders')
+                    ->selectRaw('*, sum(quantity_total) as sum')
+                    ->where('o_status','<>',9)
+                    ->where('active','=',1)
+                    ->whereDate('delivery_date','<',$today)
+                    ->groupBy('delivery_date')
+//                    ->sum('quantity_total');
+                    ->get();
+        
+        return view('admin/delivery_schedule',compact('orders','total','lates','tot_lates'));
     }
 }
