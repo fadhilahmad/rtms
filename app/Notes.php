ff57177ba1ae752f4f6aa4d1f01178fba4e77d5a
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-29 05:25:51 +0800

    notes model

---
+++ app/Notes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notes extends Model
+{
+    //
+    protected $table = 'notes';
+    protected $primaryKey = 'note_id';
+}
