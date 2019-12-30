6db607957133067d2060c1f8d26421d772f41aa2
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-30 10:04:19 +0800

    discount model

---
+++ app/Discount.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discount extends Model
+{
+    //
+    protected $table = 'discount';
+    protected $primaryKey = 'dis_id';
+}
