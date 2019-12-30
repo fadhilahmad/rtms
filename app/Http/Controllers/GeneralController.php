f3b0b89b89c8ce9d4488275fd821d73a1fca31e8
Fadhil Ahmad <fadhil.ahmad2323@gmail.com>
2019-12-30 10:03:45 +0800

    general controller updated

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -9,6 +9,12 @@ use App\Unit;
 use App\Invoice;
 use App\Receipt;
 use App\InvoicePermanent;
+use App\SystemSetting;
+use App\BankDetail;
+use App\ContactNumber;
+use App\Discount;
+use App\AdditionalCharges;
+use Illuminate\Support\Facades\Redirect;
 
 class GeneralController extends Controller
 {
@@ -84,8 +90,20 @@ class GeneralController extends Controller
                 ->first();
              
              $invoice_p = InvoicePermanent::all();
+             
+             $oid = $o_id;
+             
+             $settings = SystemSetting::first();
+             
+             $banks = BankDetail::all();
+             
+             $contacts = ContactNumber::all();
+                                      
+             $discounts = DB::table('discount')
+                       ->where('o_id','=',$o_id)
+                       ->get();
       
-        return view('invoice',compact('orders','specs','user','units','invoice','invoice_p','charges'));
+        return view('invoice',compact('orders','specs','user','units','invoice','invoice_p','charges','oid','settings','banks','contacts','discounts'));
     }
     
     public function ViewReceipt($id)
@@ -97,7 +115,132 @@ class GeneralController extends Controller
                 ->where('receipt.re_id','=',$id)
                 ->first();
             
+            $settings = SystemSetting::first();
+             
+             $banks = BankDetail::all();
+             
+             $contacts = ContactNumber::all();
+            
             //dd($orders);
-            return view('receipt',compact('receipts'));        
+            return view('receipt',compact('receipts','settings','banks','contacts'));        
+    }
+    
+    public function AlterPrice(Request $request)
+    {
+        $data = $request->all();
+        
+        if($data['operation'] == 'delete_charge')
+        {
+            //dapatkan charges, balance tolak charges, total_price tolak charges, update table orders dan invoice kemudian delete charges
+            $oid = $data['oid'];
+            $ac_id = $data['ac_id'];
+            
+            $order = DB::table('orders')
+                    ->where('o_id',$oid)
+                    ->first();
+            
+            $charge = DB::table('additional_charges')
+                    ->where('ac_id',$ac_id)
+                    ->first();
+            
+            $invoice = DB::table('invoice')
+                    ->where('o_id',$oid)
+                    ->first();
+
+            $bal = $order->balance;
+            $cas = $charge->charges;
+            $inv = $invoice->total_price;
+            
+            $balance_order = $bal - $cas;
+            $balance_invoice = $inv - $cas;
+            
+            DB::table('orders')
+                        ->where('o_id', '=', $oid)
+                        ->update(array('balance' => $balance_order,'updated_at'=>DB::raw('now()')));
+
+            DB::table('invoice')
+                        ->where('o_id', '=', $oid)
+                        ->update(array('total_price' => $balance_invoice,'updated_at'=>DB::raw('now()')));
+            
+            $delete = AdditionalCharges::find($ac_id);
+            $delete->delete();
+            
+            return Redirect::back()->with(['msg', 'Additional charge deleted']);
+        }
+        if($data['operation'] == 'add_discount')
+        {
+            $desc = $data['discount'];
+            $amount = $data['amount'];
+            $oid = $data['oid'];
+
+            $order = DB::table('orders')
+                    ->where('o_id',$oid)
+                    ->first();
+            
+            $invoice = DB::table('invoice')
+                    ->where('o_id',$oid)
+                    ->first();
+
+            $bal = $order->balance;
+            $inv = $invoice->total_price;
+            $balance_order = $bal - $amount;
+            $balance_invoice = $inv - $amount;
+
+            DB::table('discount')->insert([
+                         'o_id' => $oid,
+                         'dis_desc'=> $desc,
+                         'dis_amount'=> $amount,
+                         'created_at' => DB::raw('now()'),
+                         'updated_at' => DB::raw('now()')
+                        ]);
+
+            DB::table('orders')
+                        ->where('o_id', '=', $oid)
+                        ->update(array('balance' => $balance_order,'updated_at'=>DB::raw('now()')));
+
+            DB::table('invoice')
+                        ->where('o_id', '=', $oid)
+                        ->update(array('total_price' => $balance_invoice,'updated_at'=>DB::raw('now()')));
+            
+            return Redirect::back()->with(['msg', 'Discount added']);
+            
+        }
+        if($data['operation'] == 'delete_discount')
+        {
+            $oid = $data['oid'];
+            $dis_id = $data['dis_id'];
+            
+            $order = DB::table('orders')
+                    ->where('o_id',$oid)
+                    ->first();
+            
+            $discount = DB::table('discount')
+                    ->where('dis_id',$dis_id)
+                    ->first();
+            
+            $invoice = DB::table('invoice')
+                    ->where('o_id',$oid)
+                    ->first();
+
+            $bal = $order->balance;
+            $dis = $discount->dis_amount;
+            $inv = $invoice->total_price;
+            
+            $balance_order = $bal + $dis;
+            $balance_invoice = $inv + $dis;
+            
+            DB::table('orders')
+                        ->where('o_id', '=', $oid)
+                        ->update(array('balance' => $balance_order,'updated_at'=>DB::raw('now()')));
+
+            DB::table('invoice')
+                        ->where('o_id', '=', $oid)
+                        ->update(array('total_price' => $balance_invoice,'updated_at'=>DB::raw('now()')));
+            
+            $delete = Discount::find($dis_id);
+            $delete->delete();
+            
+            return Redirect::back()->with(['msg', 'Discount deleted']);
+        }
     }
 }
